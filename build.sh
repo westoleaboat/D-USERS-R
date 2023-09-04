fda7ba584d75cc6662d70f7c778e89cb75272c5d
atlasatlas <68698872+westoleaboat@users.noreply.github.com>
2023-09-04 23:41:42 +0300

    (ADD) management command to create first user

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ set -o errexit
 
 poetry install
 
+# Create the initial superuser before migrating
+python manage.py create_initial_superuser
+
 python manage.py collectstatic --no-input
 python manage.py migrate
