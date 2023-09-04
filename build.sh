94ed7e7be95a7ef9ad289a32e29eff91eac6f14e
atlasatlas <68698872+westoleaboat@users.noreply.github.com>
2023-09-04 23:55:34 +0300

    fix build config

--- build.sh
+++ build.sh
@@ -6,9 +6,11 @@ set -o errexit
 
 poetry install
 
-# Create the initial superuser before migrating
-python manage.py create_initial_superuser
+
 
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create the initial superuser before migrating
+python manage.py create_initial_superuser
