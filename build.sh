f7ef7d572c630b2dbd3b6d986195550d1ec91135
atlasatlas <68698872+westoleaboat@users.noreply.github.com>
2023-09-04 23:46:23 +0300

    (FIX) build script

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 # exit on error
 set -o errexit
 
-poetry install
-
 # Create the initial superuser before migrating
 python manage.py create_initial_superuser
+
+poetry install
+
 
 python manage.py collectstatic --no-input
 python manage.py migrate
